bbd572dfc9c811411e0ca2942e047316e23373bb
Tran Ngoc Lam <tranngoclam288@gmail.com>
2016-02-08 09:36:24 +0700

    init

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lamtn on 2/8/16.
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ShopApp from "./components/ShopApp";
+ReactDOM.render(<ShopApp/>, document.getElementById('app'));
